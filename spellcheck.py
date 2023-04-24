d0bd6a3ab111013b96771f1d69ef3c4686aa98d7
Nikitha2005 <84096712+Nikitha2005@users.noreply.github.com>
2023-04-24 10:15:28 -0600

    Update spellcheck.py

--- spellcheck.py
+++ spellcheck.py
@@ -1,84 +1,44 @@
-# # Spell Check Starter
-# # This start code creates two lists
-# # 1: dictionary: a list containing all of the words from "dictionary.txt"
-# # 2: aliceWords: a list containing all of the words from "AliceInWonderland.txt"
-
-# import re  # Needed for splitting text with a regular expression
-
-# dictionary = ""
-# aliceWords = ""
-
-# def main():
-#     # Load data files into lists
-#     dictionary = loadWordsFromFile("data-files/dictionary.txt")
-#     aliceWords = loadWordsFromFile("data-files/AliceInWonderLand.txt")
-
-#     # Print first 50 values of each list to verify contents
-#     print(dictionary[0:50])
-#     print(aliceWords[0:50])
-# # end main()
-
-
-# def loadWordsFromFile(fileName):
-#     # Read file as a string
-#     fileref = open(fileName, "r")
-#     textData = fileref.read()
-#     fileref.close()
-
-#     # Split text by one or more whitespace characters
-#     return re.split('\s+', textData)
-# # end loadWordsFromFile()
-
-
-# # Call main() to begin program
-# main()
-
-# def selOne(anArray, item):
-#     for i in range(0, len(anArray)):
-#         if anArray[i] == item:
-#             return i
-   
-#     return -1
-
-
-# def selOne(input, dictionary, aliceWords):
-#     for i in range(0, len(dictionary)):
-#         if dictionary[i] == input:
-#             return i
-#     print()
-    
-#     return -1
-    
-
-# #  Main Menu
-# print("Main Menu")
-# print("1: Spell Check a Word(Linear Search)")
-# print("2: Spell Check a Word(Binary Search)")
-# print("3: Spell Check Alice in Wonderland(Linear Search)")
-# print("4: Spell Check Alice in Wonderland(Binary Search)")
-# print("5: EXIT")
-
-# selection = input("Enter Selection (1-5): ")
-
-# # Selection Return Order
-# if(selection == "1"):
-#     input = input("Enter a Word to search:")
-#     print(selOne(input, dictionary, aliceWords))
-
 # Spell Check Starter
 # This start code creates two lists
 # 1: dictionary: a list containing all of the words from "dictionary.txt"
 # 2: aliceWords: a list containing all of the words from "AliceInWonderland.txt"
 
-import re
-import bisect
+import re  # Needed for splitting text with a regular expression
+import time
 
-dictionary = []
-aliceWords = []
+def linearSearch(anArray, item):
+    for i in range(0, len(anArray)):
+        if anArray[i] == item:
+            return i
+   
+    return -1
 
+def binarySearch(anArray, item):
+    low = 0
+    
+    high = len(anArray) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if anArray[mid] == item:
+            return mid
+        elif anArray[mid] > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    
+    return -1
+
+def loadWordsFromFile(fileName):
+    # Read file as a string
+    fileref = open(fileName, "r")
+    textData = fileref.read()
+    fileref.close()
+
+    # Split text by one or more whitespace characters
+    return re.split('\s+', textData)
+# end loadWordsFromFile()
 
 def main():
-    global dictionary, aliceWords
     # Load data files into lists
     dictionary = loadWordsFromFile("data-files/dictionary.txt")
     aliceWords = loadWordsFromFile("data-files/AliceInWonderLand.txt")
@@ -86,79 +46,72 @@ def main():
     # Print first 50 values of each list to verify contents
     print(dictionary[0:50])
     print(aliceWords[0:50])
+    
+    print("1: Spell Check a Word (Linear Search)")
+    print("2: Spell Check a Word (Binary Search)")
+    print("3: Spell Check Alice In Wonderland (Linear Search)")
+    print("4: Spell Check Alice In Wonderland (Binary Search)")
+    print("5: Exit")
+    selection = int(input("Enter Selection:"))
 
-
-def loadWordsFromFile(fileName):
-    # Read file as a string
-    with open(fileName, "r") as file:
-        textData = file.read()
-
-    # Split text by one or more whitespace characters
-    return re.findall(r'\w+', textData.lower())
-
-
-def linear_search(input_word, word_list):
-    for i in range(len(word_list)):
-        if word_list[i] == input_word:
-            return i
-    return -1
-
-
-def binary_search(input_word, word_list):
-    index = bisect.bisect_left(word_list, input_word)
-    if index != len(word_list) and word_list[index] == input_word:
-        return index
-    else:
-        return -1
-
-
-def check_spelling(text, dictionary, search_func):
-    misspelled_words = set()
-    for word in text:
-        if search_func(word, dictionary) == -1:
-            misspelled_words.add(word)
-    print("Misspelled words:")
-    print(misspelled_words)
-
-
-if __name__ == '__main__':
-    main()
-
-    # Main Menu
-    while True:
-        print("Main Menu")
-        print("1: Spell Check a Word (Linear Search)")
-        print("2: Spell Check a Word (Binary Search)")
-        print("3: Spell Check Alice in Wonderland (Linear Search)")
-        print("4: Spell Check Alice in Wonderland (Binary Search)")
-        print("5: EXIT")
-
-        selection = input("Enter Selection (1-5): ")
-
-        if selection == "1":
-            input_word = input("Enter a word to search: ")
-            result = linear_search(input_word, dictionary)
-            if result != -1:
-                print(f"'{input_word}' found at index {result} in the dictionary")
-            else:
-                print(f"'{input_word}' not found in the dictionary")
-
-        elif selection == "2":
-            input_word = input("Enter a word to search: ")
-            result = binary_search(input_word, dictionary)
-            if result != -1:
-                print(f"'{input_word}' found at index {result} in the dictionary")
-            else:
-                print(f"'{input_word}' not found in the dictionary")
-
-        elif selection == "3":
-            check_spelling(aliceWords, dictionary, linear_search)
-
-        elif selection == "4":
-            check_spelling(aliceWords, dictionary, binary_search)
-
-        elif selection == "5":
-            break
-
+    #  Selection 1: Spell Check a Word (Linear Search)
+    if selection == 1:
+        start = time.time()
+        wordAsk = input("Enter a Word:").lower()
+        process = linearSearch(dictionary,wordAsk)
+        print("Liner Search Starting....")
+        # Linear Search for word in dictionary and output results
+        if process == -1:
+            print(wordAsk, "was not found in dictionary")
         else:
-            print("Invalid selection. Try again.")
+            print(wordAsk, "was found at position", process)
+        end = time.time()
+        print(end - start)
+            
+    # Selection 2: Spell Check a Word (Binary Search) 
+    elif selection == 2:
+        start1 = time.time()
+        wordAsk = input("Enter a Word:").lower()
+        process = binarySearch(dictionary,wordAsk)
+        print("Binary Search Starting....")
+        # Binary Search for word in dictionary and output results
+        if process == -1:
+            print(wordAsk, "was not found in dictionary")
+        else:
+            print(wordAsk, "was found at position", process)
+        end1 = time.time()
+        print(end1 - start1)
+
+    # Seleciton 3: Alice Words (Linear Search)
+    elif selection == 3:
+        start2 = time.time()
+        count = 0
+        for i in range(len(aliceWords)):
+            # Linear serch for alice words in dictionary and cound words not found
+            process = linearSearch(dictionary, aliceWords[i].lower())
+            if process == -1:
+                count += 1
+           
+        print(count, "words not found in dictionary")
+        end2 = time.time()
+        print(end2 - start2)
+        
+    
+    # Selection 4: ALice Words (Binary Search)
+    elif selection == 4:
+        start3 = time.time()
+        count = 0
+        for i in range(len(aliceWords)):
+            process = binarySearch(dictionary, aliceWords[i].lower())
+            # Binary serch for alice words in dictionary and cound words not found
+            if process == -1:
+                count += 1
+        print(count,"number of words not found in dictionary")
+        end3 = time.time()
+        print(end3 - start3)
+        
+    print("EXIT")
+   
+
+# Call main() to begin program
+main()
